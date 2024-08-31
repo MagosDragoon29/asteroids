fb140235188dbd528c96c28bca98164201013bab
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-08-31 12:07:14 -0400

    added Player rendering and turning functionality, no forward movemen yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 
@@ -7,7 +8,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0,0,0)
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,8 +19,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
     if __name__ == "__main__":
         main()
 
