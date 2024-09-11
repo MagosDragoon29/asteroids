0b4efbdfa6b058eea06f1d9fd4109f609cd2ed5c
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-09-11 17:01:36 -0400

    added scoring system, will also print to the console with the game over message

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from player import Player, Shot
 from asteroid import Asteroid
 from asteroidfield import *
 
+def display_score(screen, score):
+    font = pygame.font.Font(None, 30)
+    score_surface = font.render(f'Score: {score}', True, (255, 255, 255))
+    screen.blit(score_surface, (10,10))
 
 
 def main():
@@ -13,6 +17,7 @@ def main():
     black = (0,0,0)
     clock = pygame.time.Clock()
     dt = 0
+    score = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -34,18 +39,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         for thing in updatable:
             thing.update(dt)
         for asteroid in asteroids: 
             if asteroid.collision_check(player) == True:
                 print("Game over!")
+                print(f"Final Score: {score}")
                 pygame.quit()
                 sys.exit()
             for shot in shots: 
                 if asteroid.collision_check(shot) == True:
+                    score += 1
                     asteroid.split()
                     shot.kill()
         screen.fill(black)
+        display_score(screen, score)
         for agent in drawable:
             agent.draw(screen)
         pygame.display.flip()
